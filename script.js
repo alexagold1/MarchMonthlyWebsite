271fa3abac33bc13fe8ab9ae4703580350ae9ca1
alexagold1 <526agold@frhsd.com>
2025-03-20 09:32:42 -0400

    made stuff disappear

---
+++ script.js
@@ -0,0 +1,53 @@
+const wordList = [
+    'gold',
+    'luck',
+    'clover',
+    'rain',
+    'charm',
+    'parade',
+    'leprechaun',
+    'treasure',
+    'celebration',
+    'greenery',
+    'shenanigans',
+    'tradition'
+]
+
+// setting game variables
+
+let selectedWord = ''
+let displayWord = ''
+let wrongGuess = 0
+let guessedLetters = []
+const maxMistakes = 6
+
+function startGame(level){
+    selectedWord = getRandomWord(level)
+
+
+    // hide difficulty selection and show game name area
+
+    // add d block to the #difficultyselection 
+document.getElementById('difficultySelection').classList.add('d-none')
+    // remove d-none from difficulty box & #gamearea
+    document.getElementById('gameArea').classList.remove('d-none')
+    document.getElementById('difficultyBox').classList.remove('d-none')
+    // add d-block to #difficultyBox & #gameArea
+
+    document.getElementById('gameArea').classList.add('d-block')
+    document.getElementById('difficultyBox').classList.add('d-block')
+
+
+
+
+}
+
+function getRandomWord(level){
+let filteredWords = wordList.filter(word => {
+    if(level === 'easy') return word.length <= 4
+    if(level === 'medium') return word.length >=5 && word.length <=7
+    if(level === 'hard') return word.length >=8
+})
+
+return filteredWords[Math.floor(Math.random() * filteredWords.length)]
+}
